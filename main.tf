4bee77c1749c471a18a10324ccdcffbaa6986771
snkotian1 <99646217+snkotian1@users.noreply.github.com>
2026-07-13 14:39:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,19 +24,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-providers.tf
-
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-  backend "azurerm" {} # Left empty; populated dynamically by Azure DevOps
-}
-
 provider "azurerm" {
   features {}
 }
